76f719a00f2c16b54493057aeeaa2787f5688f31
afwfv <773360093@qq.com>
2021-04-23 19:05:24 +0800

    gx

--- v4mb.sh
+++ v4mb.sh
@@ -1,7 +1,9 @@
 wget https://ghproxy.com/https://raw.githubusercontent.com/afwfv/dd/main/v4mb.tar.gz
 tar -zxvf v4mb.tar.gz
 rm -f v4mb.tar.gz
-mv v4mb/config/* /config
+mv v4mb/config/panel/* config/panel
+mv v4mb/config/diy.sh config/diy.sh
+mv v4mb/config/auth.json config/auth.json
 rm -rf v4mb
 cd panel
 npm i
