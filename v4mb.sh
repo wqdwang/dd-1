0a31dd83129f6dd6ecebfc5a94cb05f6f8d92578
afwfv <773360093@qq.com>
2021-04-23 18:51:07 +0800

    更新

--- v4mb.sh
+++ v4mb.sh
@@ -1,8 +1,8 @@
 wget https://ghproxy.com/https://raw.githubusercontent.com/afwfv/dd/main/v4mb.tar.gz
 tar -zxvf v4mb.tar.gz
 rm -f v4mb.tar.gz
-mv v4mb/panel panel
-mv v4mb/diy.sh config/diy.sh
+mv v4mb/config/panel panel
+mv v4mb/config/diy.sh config/diy.sh
 mv v4mb/config/auth.json config/auth.json
 rm -rf v4mb
 cd panel
