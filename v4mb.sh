4c6382f37bac6cd2a06b92b18ce1b26fbb11c3f6
afwfv <773360093@qq.com>
2021-04-23 19:59:17 +0800

    更新

--- v4mb.sh
+++ v4mb.sh
@@ -1,12 +1,10 @@
 wget https://ghproxy.com/https://raw.githubusercontent.com/afwfv/dd/main/v4mb.tar.gz
 tar -zxvf v4mb.tar.gz
 rm -f v4mb.tar.gz
-mkdir -p config/panel
-mv v4mb/config/panel/* config/panel
 mv v4mb/config/diy.sh config/diy.sh
 mv v4mb/config/auth.json config/auth.json
 rm -rf v4mb
-cd config/panel
+cd panel
 npm i
 pm2 start server.js
 echo -e "端口5678，默认用户名admin，密码adminadmin"
