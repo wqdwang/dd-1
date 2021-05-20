37b08266f8a93b02172868eac1399567b61418da
wqdwang <55976516+wqdwang@users.noreply.github.com>
2021-05-20 10:44:38 +0800

    Update v4mb.sh

--- v4mb.sh
+++ v4mb.sh
@@ -1,10 +1,7 @@
-wget https://ghproxy.com/https://raw.githubusercontent.com/afwfv/dd/main/v4mb.tar.gz
-tar -zxvf v4mb.tar.gz
-rm -f v4mb.tar.gz
-mv v4mb/panel panel
-mv v4mb/config/diy.sh config/diy.sh
-mv v4mb/config/auth.json config/auth.json
-rm -rf v4mb
+tar -zxvf v4mb.tar
+rm -f v4mb.tar
+mv panel/diy.sh config/diy.sh
+mv panel/auth.json config/auth.json
 cd panel
 npm i
 pm2 start server.js
